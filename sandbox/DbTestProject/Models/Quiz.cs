6713c30af574806863dc84c04aa4ebb380da10e1
Roman Demchuk <2001.demchuk@gmail.com>
2020-10-30 15:02:36 +0200

    #27 change models

--- sandbox/DbTestProject/Models/Quiz.cs
+++ sandbox/DbTestProject/Models/Quiz.cs
@@ -8,22 +8,30 @@ namespace DbTestProject.Models
     {
         [Key]
         public int Id { get; set; }
-        
-        [Required]
-        public int UserId { get; set; }
 
         [Required]
+        public string OwnerUserId { get; set; }
+
+        [ForeignKey("OwnerUserId")]
+        public User OwnerUser { get; set; }
+
+        [Required, MinLength(1), MaxLength(50)]
         public string Title { get; set; }
 
-        [Required]
+        [Required, Range(2, 25)]
         public int PlayTime { get; set; }
 
-        [Required]
+        [Required, Range(10, int.MaxValue)]
         public int AnswerTime { get; set; }
 
-        public int GenreId { get; set; }
+        public string GenreId { get; set; }
 
         public Genre Genre { get; set; }
         public List<QuizSong> QuizSongs { get; set; }
+
+        public Quiz()
+        {
+            QuizSongs = new List<QuizSong>();
+        }
     }
 }
--- sandbox/DbTestProject/Models/User.cs
+++ sandbox/DbTestProject/Models/User.cs
@@ -5,17 +5,23 @@ namespace DbTestProject.Models
 {
     public class User
     {
-        [Key]
-        public int Id { get; set; }
-
-        [Required]
+        [Key, MinLength(2), MaxLength(50)]
         public string Username { get; set; }
         
-        [Required] 
+        [Required, MinLength(4), MaxLength(255)] 
         public string Password { get; set; }
-
+        
+        [Range(1,5)]
+        public int? AppRating { get; set; }
+        
         public List<Quiz> Quizzes { get; set; }
         public List<Score> Scores { get; set; }
-        public int AppRating { get; set; }
+
+
+        public User()
+        {
+            Quizzes = new List<Quiz>();
+            Scores = new List<Score>();
+        }
     }
 }
