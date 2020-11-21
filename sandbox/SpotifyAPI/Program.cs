5a8951740865838e20787a8d1aa5bf5fb30d56f7
dmytroshurko <dmysh@protonmail.com>
2020-11-21 18:44:26 +0200

    add initial code (spotify api)

--- sandbox/SpotifyAPI/Program.cs
+++ sandbox/SpotifyAPI/Program.cs
@@ -1,12 +1,28 @@
 ﻿using System;
+using System.Threading.Tasks;
+using SpotifyAPI.Web;
+
 
 namespace SpotifyAPI
 {
     class Program
     {
-        static void Main(string[] args)
+        
+
+        static async Task Main()
         {
-            Console.WriteLine("Hello World!");
+            var config = SpotifyClientConfig.CreateDefault();
+
+            var request = new ClientCredentialsRequest("3fd87b1e985c4733b30f6f387138b3ef", "615c188b29534b0c9e2f74c63b93de20");
+            var response = await new OAuthClient(config).RequestToken(request);
+
+            var spotify = new SpotifyClient(config.WithToken(response.AccessToken));
+
+
+
+            var track = await spotify.Tracks.Get("0sfdiwck2xr4PteGOdyOfz");
+            Console.WriteLine(track.PreviewUrl);
         }
+
     }
 }
