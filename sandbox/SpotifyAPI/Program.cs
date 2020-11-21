d2ab2f4094809107cf233028fd111d0c55827ad8
dmytroshurko <dmysh@protonmail.com>
2020-11-21 20:43:11 +0200

    add function for retrieving songs ids from playlist

--- sandbox/SpotifyAPI/Program.cs
+++ sandbox/SpotifyAPI/Program.cs
@@ -1,16 +1,14 @@
 ﻿using System;
 using System.Threading.Tasks;
 using SpotifyAPI.Web;
+using System.Collections.Generic;
 
 
 namespace SpotifyAPI
 {
     class Program
     {
-        
-
-        static async Task Main()
-        {
+        static async Task<List<string>> GetSongsFromPlaylist(string playlistId) {
             var config = SpotifyClientConfig.CreateDefault();
 
             var request = new ClientCredentialsRequest("3fd87b1e985c4733b30f6f387138b3ef", "615c188b29534b0c9e2f74c63b93de20");
@@ -18,10 +16,36 @@ namespace SpotifyAPI
 
             var spotify = new SpotifyClient(config.WithToken(response.AccessToken));
 
+            var playlist = await spotify.Playlists.Get(playlistId);
+
+            List<string> songsIds = new List<string>();
+
+            foreach (PlaylistTrack<IPlayableItem> item in playlist.Tracks.Items)
+            {
+                if (item.Track is FullTrack track)
+                {
+                    string url = track.PreviewUrl;
+                    if (!string.IsNullOrWhiteSpace(url))
+                    {
+                     songsIds.Add(track.Id);
+                    }
+                }
+            }
+            
+            return songsIds;
+        }
 
 
-            var track = await spotify.Tracks.Get("0sfdiwck2xr4PteGOdyOfz");
-            Console.WriteLine(track.PreviewUrl);
+
+        static async Task Main()
+        {
+            // Get songs ids from the playlist
+            List<string> songsIds = await GetSongsFromPlaylist("37i9dQZF1DX1rVvRgjX59F");
+
+            foreach (var item in songsIds)
+            {
+                Console.WriteLine(item);
+            }
         }
 
     }
